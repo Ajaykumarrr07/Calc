1bc2a633bb81c149765ee33a91bf4ec663f14ac1
Ajaykumarrr07 <ajaykumarrr504@gmail.com>
2025-11-15 19:17:52 +0530

    this is initial commit

---
+++ src/com/Calc/Calc.java
@@ -0,0 +1,9 @@
+package com.Calc;
+
+public interface Calc {
+	public int add (int a ,int b);
+	public double sub (int a ,int b);
+	public int mul (int a ,int b);
+	public double div (int a ,int b);
+
+}

e639472c73d24d9099bc3105f99d5eb7b3a25abe
Ajaykumarrr07 <ajaykumarrr504@gmail.com>
2025-11-17 19:16:50 +0530

    comment

---
+++ src/com/Calc/CalcImplementation.java
@@ -0,0 +1,25 @@
+package com.Calc;
+
+public class CalcImplementation  implements Calc {
+	
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+		
+	}
+	@Override
+	public double div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	} @Override
+	public double sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}

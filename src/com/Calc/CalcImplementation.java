dd31b2a062088547d44bc429387a17cfb7d8b919
indurmaniteja444 <indurmaniteja44@gmail.com>
2025-11-18 18:22:56 +0530

    Implementation for divison

--- src/com/Calc/CalcImplementation.java
+++ src/com/Calc/CalcImplementation.java
@@ -10,8 +10,9 @@ public class CalcImplementation  implements Calc {
 	}
 	@Override
 	public double div(int a, int b) {
-		// TODO Auto-generated method stub
-		return 0;
+		// Feature implemented by mani
+		int c=a/b;
+		return c;
 	}
 	@Override
 	public int mul(int a, int b) {

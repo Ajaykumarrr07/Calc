197c8ab053a8383618b94ac04ba0fc03c8875117
Ajaykumarrr07 <ajaykumarrr504@gmail.com>
2025-11-19 08:02:16 +0530

    SWITCHCASE ADDED IMPL LAYER

---
+++ src/com/Calc/Drivered.java
@@ -0,0 +1,51 @@
+package com.Calc;
+
+import java.util.Scanner;
+
+public class Drivered {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        CalcImplementation calc = new CalcImplementation();
+        System.out.print("Enter the first number : ");
+        int first = scan.nextInt();
+        System.out.print("Enter the second number : ");
+        int second = scan.nextInt();
+
+        System.out.println("----------------------------------------------");
+        System.out.println("1 is +");
+        System.out.println("2 is -");
+        System.out.println("3 is *");
+        System.out.println("4 is /");
+        System.out.print("Enter your choice : ");
+        int choice = scan.nextInt();
+
+        switch (choice) {
+            case 1: {
+                // add operation here
+            }
+            break;
+
+            case 2: {
+                // subtract operation here
+            }
+            break;
+
+            case 3: {
+                // multiply operation here
+            }
+            break;
+
+            case 4: {
+                // divide operation here
+            }
+            break;
+
+            default: {
+                System.out.println("------------INVALID CHOICE-------------");
+            }
+        }
+
+     
+    }
+}
